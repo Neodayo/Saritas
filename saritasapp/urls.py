abc418fe1cc136c6ff88c2f690abcf3cc1cc337f
NurAli <gt201901065@wmsu.edu.ph>
2025-02-26 11:54:20 +0800

    django project saritas

---
+++ saritasapp/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views  
+
+app_name = 'saritasapp'
+
+urlpatterns = [
+    path('add-inventory/', views.add_inventory, name='add_inventory'),
+    path('inventory/', views.inventory_view, name='inventory_list'),
+    path('add-category/', views.add_category, name='add_category'),
+    path('inventory/<int:item_id>/', views.view_inventory, name='view_inventory'),
+    path('inventory/<int:item_id>/edit/', views.edit_inventory, name='edit_inventory'), 
+    path('inventory/<int:item_id>/delete/', views.delete_inventory, name='delete_inventory'),
+    path('rental/', views.rent_item, name='rental'),
+    path('customer/create/', views.create_customer, name='create_customer'),
+]
