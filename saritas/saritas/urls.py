48d49b7cbf544cc6159c3a392ef719321c381568
c_keiprx <mrlskarel@gmail.com>
2025-04-04 23:53:49 +0800

    changes in login directory

--- saritas/saritas/urls.py
+++ saritas/saritas/urls.py
@@ -21,7 +21,7 @@ from customerapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("staff", include('saritasapp.urls', namespace='saritasapp')),
+    path("", include('saritasapp.urls', namespace='saritasapp')),
     path("customer", include("customerapp.urls", namespace="customerapp")),
     path('', views.homepage, name='homepage'),
 ]
